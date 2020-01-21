a677c728de1f725e79095df9185530528f608c16
David Darias <dvd.darias@gmail.com>
2020-01-21 11:23:43 -0500

    Better user detection

--- js/panel.js
+++ js/panel.js
@@ -3,13 +3,13 @@ $(function () {
     
     //on beds24 panel config there should be the following config variables:
     if(    !panel_custom_settings 
-        || !panel_custom_settings.users 
+        || !panel_custom_settings.allowed_users 
+        || !panel_custom_settings.user 
         || !panel_custom_settings.filter
         ){
         return;
     }
 
-    const users = panel_custom_settings.users;
     const filter = panel_custom_settings.filter;    
 
     //add a new containsi expression to filter case-insensitive
@@ -26,40 +26,34 @@ $(function () {
         });
     }
 
-    //check if is a valid user
-    var allowed = false;
-    for (let i = 0; i < users.length; i++) {
-        const name = users[i];
-        if ($("span.hide_helplink:containsi('" + name + "')").length > 0) {
-            allowed = true;
-            break;
-        }
-    }
+    //check if is an allowed user
+    var allowed = panel_custom_settings.allowed_users.indexOf(panel_custom_settings.user)>=0;
+    if(!allowed) return;
+    
+
     //if allowed iterate over filtered bookings and replace contents with hover text
-    if (allowed) {
-        $("#calendartableholder [data-bookid] div:containsi('" + filter + "')").each(function (index) {
-            const bookElement = $(this).parent();
-            const bookid = bookElement.attr("data-bookid");
-            const element = $(this);
+    $("#calendartableholder [data-bookid] div:containsi('" + filter + "')").each(function (index) {
+        const bookElement = $(this).parent();
+        const bookid = bookElement.attr("data-bookid");
+        const element = $(this);
 
-            //set the color to semitransparent
-            var color = bookElement.css("background-color");
-            color = color.replace("rgb", "rgba").replace(")", ",0.75)");
-            bookElement.css("background-color", color);
-            //hide the text on the booking
-            bookElement.css("color", "rgba(0,0,0,0)");
+        //set the color to semitransparent
+        var color = bookElement.css("background-color");
+        color = color.replace("rgb", "rgba").replace(")", ",0.75)");
+        bookElement.css("background-color", color);
+        //hide the text on the booking
+        bookElement.css("color", "rgba(0,0,0,0)");
 
-            //get the hover text and replace it
-            $.ajax({
-                url: "/api/ajax/bookinghover.php?b=" + bookid,
-                success: function (result) {
-                    element.textNodes().remove();
-                    element.find("br").remove();
-                    element.append("&nbsp;" + result);
-                    //show the text on the booking
-                    bookElement.css("color", "rgb(0,0,0)");
-                }
-            });
+        //get the hover text and replace it
+        $.ajax({
+            url: "/api/ajax/bookinghover.php?b=" + bookid,
+            success: function (result) {
+                element.textNodes().remove();
+                element.find("br").remove();
+                element.append("&nbsp;" + result);
+                //show the text on the booking
+                bookElement.css("color", "rgb(0,0,0)");
+            }
         });
-    }
+    });
 });
