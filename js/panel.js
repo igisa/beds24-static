e10ee7a6611ce6e6d7be430ee961ca442edb0019
David Darias <dvd.darias@gmail.com>
2020-01-22 12:33:09 -0500

    Support of icon colors

--- js/panel.js
+++ js/panel.js
@@ -26,7 +26,36 @@ $(function () {
         });
     }
 
-    //check if is a valid user
+    $("#calendartableholder [data-bookid] div i").each(function (index) {
+        const iconElement = $(this);        
+        iconElement.css("background-color","#ffffff");
+        iconElement.css("padding", "3px");
+        iconElement.css("border-radius","4px");
+
+        var text = iconElement.attr("title");
+        
+        if(text && text.startsWith("#")){
+            var split = text.split(" ");
+            if(split[0]){
+                var color = split[0];
+                iconElement.css("color", `${color}`); 
+                iconElement.attr("title", text.substr(split[0].length, text.length - split[0].length));
+            }
+        }
+
+        const iconParent = $(this).parent();
+        iconParent.css("margin-left", "5px");
+        iconParent.css("margin-right", "5px");
+
+        if(text && text.indexOf("Cobrado: Si")>=0){
+            var moneyIcon = $(`
+                <i class = "fas fa-dollar-sign" style = "font-size: 80%; color: #45ab45; margin-left: 3px;"></i>
+            `);
+            iconElement.append(moneyIcon);
+        }
+    });
+
+    //check if is a valid user for booking text-tooltip replacement
     var allowed = false;
     for (let i = 0; i < users.length; i++) {
         const name = users[i];
