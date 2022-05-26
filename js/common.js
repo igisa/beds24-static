fba16b6234408b1e2cde0f734fde15172a09843e
David Darias <dvd.darias@gmail.com>
2022-05-26 17:08:28 -0400

    Bugfix

--- js/common.js
+++ js/common.js
@@ -134,6 +134,28 @@ booking_extras.fields = {
         ],
         is_number: true,
     },
+    numNights: {
+        label: "Noches",
+        type: "options",
+        options: [
+            { id: "1", label: "1" },
+            { id: "2", label: "2" },
+            { id: "3", label: "3" },
+            { id: "4", label: "4" },
+            { id: "5", label: "5" },
+            { id: "6", label: "6" },
+            { id: "7", label: "7" },
+            { id: "8", label: "8" },
+            { id: "9", label: "9" },
+            { id: "10", label: "10" },
+            { id: "11", label: "11" },
+            { id: "12", label: "12" },
+            { id: "13", label: "13" },
+            { id: "14", label: "14" },
+            { id: "15", label: "15" },
+        ],
+        is_number: true,
+    },
     country:{
         label: "País",
         type: "text",
@@ -194,7 +216,7 @@ booking_extras.services = {
 
     add_night: {
         description: "Noches Adicionales",
-        description_short: "Additional.NIGHT",
+        description_short: "Add.NIGHT",
         price: {
             update_on: ["numNights"],
             cost: "$numNights * 0",
